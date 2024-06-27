46a28d2bd12676d0b62ad3ec33658d3afa23e880
Danish Khan <danish.khan@ebpearls.com>
2024-06-27 16:48:07 +0545

    Setup/Create bloc for sign up

---
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../domain/usecases/user_sign_up.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final UserSignUp _userSignUp;
+  AuthBloc({
+    required UserSignUp userSignUp,
+  })  : _userSignUp = userSignUp, // this is called an initializer list
+        super(AuthInitial()) {
+    on<AuthEvent>((_, emit) => emit(AuthLoading()));
+    on<AuthSignUp>(_onAuthSignUp);
+  }
+
+  void _onAuthSignUp(
+    AuthSignUp event,
+    Emitter<AuthState> emit,
+  ) async {
+    final res = await _userSignUp(
+      UserSignUpParams(
+        email: event.email,
+        password: event.password,
+        name: event.name,
+      ),
+    );
+
+    res.fold(
+      (failure) => emit(AuthFailure(failure.message)),
+      (user) => _emitAuthSuccess(user, emit),
+    );
+  }
+
+  void _emitAuthSuccess(
+    String userId,
+    Emitter<AuthState> emit,
+  ) {
+    emit(AuthSuccess(userId));
+  }
+}
