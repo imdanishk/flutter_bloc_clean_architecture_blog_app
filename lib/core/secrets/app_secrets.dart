8b41f8b8f1ee18ff2c014246a2b0f95bd75b9fd5
Danish Khan <danish.khan@ebpearls.com>
2024-06-27 15:13:51 +0545

    Setup supabase

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://agqkqyrrjwzknxconznm.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFncWtxeXJyand6a254Y29uem5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NzkxOTMsImV4cCI6MjAzNTA1NTE5M30.G-Xidxol8_L8wVslVWMMTXRULr0cFMQuYB3GNI9OmWo';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_clean_architecture_blog_app/core/secrets/app_secrets.dart';
 import 'package:flutter_bloc_clean_architecture_blog_app/core/theme/theme.dart';
 import 'package:flutter_bloc_clean_architecture_blog_app/features/auth/presentation/pages/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
   runApp(const MyApp());
 }
 
@@ -14,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.darkThemeMode,
       home: const LoginPage(),
     );
