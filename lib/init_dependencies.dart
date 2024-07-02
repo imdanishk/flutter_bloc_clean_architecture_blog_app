7d3048bb9f82495d82f4810d4764326fc6205b8b
Danish Khan <danish.khan@ebpearls.com>
2024-07-02 17:35:41 +0545

    Optimize imports - separate imports and file content

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,3 +1,9 @@
+import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 import 'package:flutter_bloc_clean_architecture_blog_app/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:flutter_bloc_clean_architecture_blog_app/core/network/connection_checker.dart';
 import 'package:flutter_bloc_clean_architecture_blog_app/core/secrets/app_secrets.dart';
@@ -15,130 +21,5 @@ import 'package:flutter_bloc_clean_architecture_blog_app/features/blog/domain/re
 import 'package:flutter_bloc_clean_architecture_blog_app/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:flutter_bloc_clean_architecture_blog_app/features/blog/domain/usecases/upload_blog.dart';
 import 'package:flutter_bloc_clean_architecture_blog_app/features/blog/presentation/bloc/blog_bloc.dart';
-import 'package:get_it/get_it.dart';
-import 'package:hive/hive.dart';
-import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
-final serviceLocator = GetIt.instance;
-
-Future<void> initDependencies() async {
-  _initAuth();
-  _initBlog();
-
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.supabaseUrl,
-    anonKey: AppSecrets.supabaseAnonKey,
-  );
-
-  Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
-
-  // Registering Supabase client as a lazy singleton to ensure only one instance is created and shared across the app.
-  serviceLocator.registerLazySingleton(() => supabase.client);
-
-  serviceLocator.registerLazySingleton(
-    () => Hive.box(name: 'blogs'),
-  );
-
-  serviceLocator.registerFactory(() => InternetConnection());
-
-  // core
-  serviceLocator.registerLazySingleton(
-    () => AppUserCubit(),
-  );
-  serviceLocator.registerFactory<ConnectionChecker>(
-    () => ConnectionCheckerImpl(
-      serviceLocator(),
-    ),
-  );
-}
-
-void _initAuth() {
-  // Datasource
-  serviceLocator
-    ..registerFactory<AuthRemoteDataSource>(
-      () => AuthRemoteDataSourceImpl(
-        serviceLocator(),
-      ),
-    )
-
-    // Repository
-    // Registering AuthRepository as a factory to get a new instance each time it's needed.
-    ..registerFactory<AuthRepository>(
-      () => AuthRepositoryImpl(
-        serviceLocator(),
-        serviceLocator(),
-      ),
-    )
-
-    // Usecases
-    // Registering UserSignUp use case as a factory to ensure each use case call gets a fresh instance.
-    ..registerFactory(
-      () => UserSignUp(
-        serviceLocator(),
-      ),
-    )
-    ..registerFactory(
-      () => UserLogin(
-        serviceLocator(),
-      ),
-    )
-    ..registerFactory(
-      () => CurrentUser(
-        serviceLocator(),
-      ),
-    )
-
-    // Registering AuthBloc as a lazy singleton to maintain a single instance throughout the app's lifecycle.
-    ..registerLazySingleton(
-      () => AuthBloc(
-        userSignUp: serviceLocator(),
-        userLogin: serviceLocator(),
-        currentUser: serviceLocator(),
-        appUserCubit: serviceLocator(),
-      ),
-    );
-}
-
-void _initBlog() {
-  // Datasource
-  serviceLocator
-    ..registerFactory<BlogRemoteDataSource>(
-      () => BlogRemoteDataSourceImpl(
-        serviceLocator(),
-      ),
-    )
-    ..registerFactory<BlogLocalDataSource>(
-      () => BlogLocalDataSourceImpl(
-        serviceLocator(),
-      ),
-    )
-
-    // Repository
-    ..registerFactory<BlogRepository>(
-      () => BlogRepositoryImpl(
-        serviceLocator(),
-        serviceLocator(),
-        serviceLocator(),
-      ),
-    )
-    // Usecases
-    ..registerFactory(
-      () => UploadBlog(
-        serviceLocator(),
-      ),
-    )
-    ..registerFactory(
-      () => GetAllBlogs(
-        serviceLocator(),
-      ),
-    )
-    // Bloc
-    ..registerLazySingleton(
-      () => BlogBloc(
-        uploadBlog: serviceLocator(),
-        getAllBlogs: serviceLocator(),
-      ),
-    );
-}
+part 'init_dependencies.main.dart';
---
+++ lib/init_dependencies.main.dart
@@ -0,0 +1,123 @@
+part of 'init_dependencies.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+  _initBlog();
+
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+
+  Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
+
+  // Registering Supabase client as a lazy singleton to ensure only one instance is created and shared across the app.
+  serviceLocator.registerLazySingleton(() => supabase.client);
+
+  serviceLocator.registerLazySingleton(
+    () => Hive.box(name: 'blogs'),
+  );
+
+  serviceLocator.registerFactory(() => InternetConnection());
+
+  // core
+  serviceLocator.registerLazySingleton(
+    () => AppUserCubit(),
+  );
+  serviceLocator.registerFactory<ConnectionChecker>(
+    () => ConnectionCheckerImpl(
+      serviceLocator(),
+    ),
+  );
+}
+
+void _initAuth() {
+  // Datasource
+  serviceLocator
+    ..registerFactory<AuthRemoteDataSource>(
+      () => AuthRemoteDataSourceImpl(
+        serviceLocator(),
+      ),
+    )
+
+    // Repository
+    // Registering AuthRepository as a factory to get a new instance each time it's needed.
+    ..registerFactory<AuthRepository>(
+      () => AuthRepositoryImpl(
+        serviceLocator(),
+        serviceLocator(),
+      ),
+    )
+
+    // Usecases
+    // Registering UserSignUp use case as a factory to ensure each use case call gets a fresh instance.
+    ..registerFactory(
+      () => UserSignUp(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory(
+      () => UserLogin(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory(
+      () => CurrentUser(
+        serviceLocator(),
+      ),
+    )
+
+    // Registering AuthBloc as a lazy singleton to maintain a single instance throughout the app's lifecycle.
+    ..registerLazySingleton(
+      () => AuthBloc(
+        userSignUp: serviceLocator(),
+        userLogin: serviceLocator(),
+        currentUser: serviceLocator(),
+        appUserCubit: serviceLocator(),
+      ),
+    );
+}
+
+void _initBlog() {
+  // Datasource
+  serviceLocator
+    ..registerFactory<BlogRemoteDataSource>(
+      () => BlogRemoteDataSourceImpl(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory<BlogLocalDataSource>(
+      () => BlogLocalDataSourceImpl(
+        serviceLocator(),
+      ),
+    )
+
+    // Repository
+    ..registerFactory<BlogRepository>(
+      () => BlogRepositoryImpl(
+        serviceLocator(),
+        serviceLocator(),
+        serviceLocator(),
+      ),
+    )
+    // Usecases
+    ..registerFactory(
+      () => UploadBlog(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory(
+      () => GetAllBlogs(
+        serviceLocator(),
+      ),
+    )
+    // Bloc
+    ..registerLazySingleton(
+      () => BlogBloc(
+        uploadBlog: serviceLocator(),
+        getAllBlogs: serviceLocator(),
+      ),
+    );
+}
